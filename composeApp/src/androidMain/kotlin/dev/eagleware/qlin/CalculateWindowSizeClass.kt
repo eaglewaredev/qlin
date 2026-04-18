77610bb8d91e5c1502a3e36b4a2d17c13e90b95a
eagle <eagle@aerie.dev>
2026-04-18 13:57:02 +0300

    feat: add Calculated Window Size

---
+++ composeApp/src/androidMain/kotlin/dev/eagleware/qlin/CalculateWindowSizeClass.kt
@@ -0,0 +1,13 @@
+package dev.eagleware.qlin
+
+import androidx.activity.compose.LocalActivity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+actual fun calculateWindowSizeClass(): WindowSizeClass {
+    return calculateWindowSizeClass(LocalActivity.current!!)
+}
---
+++ composeApp/src/commonMain/kotlin/dev/eagleware/qlin/CalculateWindowSizeClass.kt
@@ -0,0 +1,7 @@
+package dev.eagleware.qlin
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+
+@Composable
+expect fun calculateWindowSizeClass(): WindowSizeClass
